0fc8d4290dd4cb30d7255b73e5bf38c439bfadbd
SLUCHABLUB <81807027+SLUCHABLUB@users.noreply.github.com>
2025-02-28 17:20:59 +0100

    Added `Layers`

---
+++ daur-lib/src/widget/heterogeneous/layers.rs
@@ -0,0 +1,32 @@
+use crate::app::Action;
+use crate::ui::{Point, Rectangle};
+use crate::widget::Widget;
+use crossterm::event::MouseButton;
+use ratatui::buffer::Buffer;
+
+/// Layers of heterogeneous widgets
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub struct Layers<Children> {
+    /// The layered widgets
+    pub children: Children,
+}
+
+macro_rules! impl_layers {
+    ($len:literal; $($generic:ident),*; $($index:tt),*) => {
+        impl<$($generic: Widget),*> Widget for Layers<($($generic),*)> {
+            fn render(&self, area: Rectangle, buffer: &mut Buffer, mouse_position: Point) {
+                $(
+                    self.children.$index.render(area, buffer, mouse_position);
+                )*
+            }
+
+            fn click(&self, area: Rectangle, button: MouseButton, position: Point, actions: &mut Vec<Action>) {
+                $(
+                    self.children.$index.click(area, button, position, actions);
+                )*
+            }
+        }
+    };
+}
+
+impl_layers!(2; A, B; 0, 1);
--- daur-lib/src/widget/heterogeneous/mod.rs
+++ daur-lib/src/widget/heterogeneous/mod.rs
@@ -1,5 +1,7 @@
 //! Heterogeneous layouts of widgets
 
+mod layers;
 mod stack;
 
+pub use layers::Layers;
 pub use stack::{FourStack, Stack, ThreeStack, TwoStack};
