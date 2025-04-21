46369a95a2b483f149f9c93e32793fda5be67b34
SLUCHABLUB <81807027+SLUCHABLUB@users.noreply.github.com>
2025-04-21 10:34:58 +0200

    Move the minimum_size implementation to a function

--- daur-lib/src/view/mod.rs
+++ daur-lib/src/view/mod.rs
@@ -10,6 +10,7 @@ mod canvas;
 mod cursor;
 mod direction;
 mod feed;
+mod minimum_size;
 mod quotum;
 mod ruler;
 mod text;
@@ -26,6 +27,7 @@ pub use text::ToText;
 
 use crate::context::Menu;
 use crate::ui::{Length, Size};
+use crate::view::minimum_size::minimum_size;
 use crate::{ArcCell, Colour, Ratio, UserInterface};
 use arcstr::ArcStr;
 use derive_more::Debug;
@@ -252,77 +254,6 @@ impl View {
     /// Returns the minimum size required to fit the entire view.
     #[must_use]
     pub fn minimum_size<Ui: UserInterface>(&self) -> Size {
-        match self {
-            // TODO: this may depend on thickness
-            View::Bordered {
-                title: _,
-                thick: _,
-                content,
-            } => {
-                let mut size = content.minimum_size::<Ui>();
-                size.height += Ui::DOUBLE_BORDER_THICKNESS;
-                size.width += Ui::DOUBLE_BORDER_THICKNESS;
-                size
-            }
-            View::Button {
-                on_click: _,
-                content,
-            } => content.minimum_size::<Ui>(),
-            View::Canvas { .. }
-            | View::CursorWindow { .. }
-            | View::Empty
-            | View::FileSelector { .. }
-            | View::SizeInformed(_)
-            | View::Solid(_) => Size::ZERO,
-            View::Contextual { menu: _, view } => view.minimum_size::<Ui>(),
-            View::Generator(generator) => generator().minimum_size::<Ui>(),
-            View::Hoverable { default, hovered } => {
-                let default = default.minimum_size::<Ui>();
-                let hovered = hovered.minimum_size::<Ui>();
-
-                Size {
-                    width: max(default.width, hovered.width),
-                    height: max(default.height, hovered.height),
-                }
-            }
-            View::Layers(layers) => {
-                let mut size = Size::ZERO;
-
-                for layer in layers {
-                    let layer_size = layer.minimum_size::<Ui>();
-                    size.width = max(size.width, layer_size.width);
-                    size.height = max(size.height, layer_size.height);
-                }
-
-                size
-            }
-            View::Rule { .. } => Size {
-                width: Length::ZERO,
-                height: Ui::RULER_HEIGHT.get(),
-            },
-            View::Sized { minimum_size, .. } => *minimum_size,
-            View::Stack {
-                direction,
-                elements,
-            } => {
-                let mut parallel = Length::ZERO;
-                let mut orthogonal = Length::ZERO;
-
-                for quoted in elements {
-                    let child = quoted.view.minimum_size::<Ui>();
-                    parallel += child.parallel_to(*direction);
-                    orthogonal = max(orthogonal, child.orthogonal_to(*direction));
-                }
-
-                Size::from_parallel_orthogonal(parallel, orthogonal, *direction)
-            }
-            View::Text {
-                string,
-                alignment: _,
-            } => Size {
-                width: Ui::string_width(string),
-                height: Ui::string_height(string),
-            },
-        }
+        minimum_size::<Ui>(self)
     }
 }
