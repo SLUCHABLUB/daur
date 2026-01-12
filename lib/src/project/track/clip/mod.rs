89ab32314467bfe666525d5a9a628dcf738cb928
Nelli Skogman <nelli.skogman@gmail.com>
2026-01-12 15:15:39 +0100

    Add a private constructor for `Clip`.

--- lib/src/project/track/clip/mod.rs
+++ lib/src/project/track/clip/mod.rs
@@ -42,9 +42,11 @@ pub struct Clip {
     /// The name of the clip.
     #[get_clone = "pub"]
     name: ArcStr,
+
     /// The colour of the clip.
     #[get_copy = "pub"]
     colour: Colour,
+
     /// The content of the clip.
     #[get = "pub(crate)"]
     content: Content,
@@ -56,24 +58,27 @@ impl Clip {
         &mut self.content
     }
 
-    #[must_use]
-    pub(crate) fn from_audio(name: ArcStr, audio: FixedLength) -> Clip {
+    fn new(name: ArcStr, colour: Colour, content: Content) -> Clip {
         Clip {
             id: Id::generate(),
             name,
-            colour: DEFAULT_AUDIO_COLOUR,
-            content: Content::Audio(audio),
+            colour,
+            content,
         }
     }
 
     #[must_use]
+    pub(crate) fn from_audio(name: ArcStr, audio: FixedLength) -> Clip {
+        Clip::new(name, DEFAULT_AUDIO_COLOUR, Content::Audio(audio))
+    }
+
+    #[must_use]
     pub(crate) fn empty_notes(duration: NonZeroDuration) -> Clip {
-        Clip {
-            id: Id::generate(),
-            name: DEFAULT_NOTES_NAME,
-            colour: DEFAULT_NOTES_COLOUR,
-            content: Content::Notes(note::Group::empty(duration)),
-        }
+        Clip::new(
+            DEFAULT_NOTES_NAME,
+            DEFAULT_NOTES_COLOUR,
+            Content::Notes(note::Group::empty(duration)),
+        )
     }
 
     pub(crate) fn duration(&self) -> NonZeroDuration {
